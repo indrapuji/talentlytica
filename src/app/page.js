67d01ef77212007da6a9a84644e1cd9c95824467
Indra Puji Novirwan <indrapuji@gmail.com>
2023-06-18 17:27:53 +0700

    done

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,96 @@
+'use client'
+
+import React, {useState} from 'react'
 import Image from 'next/image'
+import profile from '@/assets/default.png'
+
+import {mahasiswa, nilai, titleAspek} from '@/utilities/data'
 
 export default function Home() {
+  const [formData, setFormData] = useState({})
+
+  const onFormChange = (e) => {
+    e.preventDefault()
+    const {name, value} = e.target
+    setFormData({
+      ...formData,
+      [name]: value
+    })
+  }
+
+  const reGroup = (data) => {
+    const result = {}
+    for (const key in data) {
+      const splitted = key.split('-')
+      const aspek = splitted[0]
+      const mahasiswa = splitted[1]
+      const nilai = data[key]
+      if (!(aspek in result)) {
+        result[aspek] = {[mahasiswa]: Number(nilai)}
+      } else {
+        result[aspek][mahasiswa] = Number(nilai)
+      }
+    }
+    return result
+  }
+
+  const onFormSubmit = async (e) => {
+    try {
+      e.preventDefault()
+      console.log(reGroup(formData))
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='inline-block w-full h-full'>
+      <div className='w-full my-8'>
+        <p className='text-center text-4xl'>Aplikasi Penilaian Mahasiswa</p>
+      </div>
+      <form onSubmit={onFormSubmit}>
+        <div className='flex flex-row p-2 m-2'>
+          <p className='flex-1 text-center mx-1'></p>
+          {titleAspek.map((item, idx) => (
+            <p key={idx} className='flex-1 text-center mx-1'>
+              {item.title}
+            </p>
+          ))}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        {mahasiswa.map((item, idx) => (
+          <div key={idx} className='flex flex-row border-solid border-2 border-gray-400 p-2 m-2'>
+            <div className='flex flex-1 items-center mx-1'>
+              <Image className='rounded-full w-8 h-8 mx-1' src={profile} alt='profile' />
+              <p>{item.title}</p>
+            </div>
+            {titleAspek.map((x, i) => (
+              <div key={i} className='relative flex-1 mx-1'>
+                <select
+                  className='block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline'
+                  name={`${x.name}-${item.name}`}
+                  onChange={onFormChange}
+                >
+                  {nilai.map((item, i) => (
+                    <option key={i} value={item.value}>
+                      {item.name}
+                    </option>
+                  ))}
+                </select>
+                <div className='pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700'>
+                  <svg className='fill-current h-4 w-4' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
+                    <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
+                  </svg>
+                </div>
+              </div>
+            ))}
+          </div>
+        ))}
+        <div className='float-right m-1'>
+          <button className='bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' type='submit'>
+            Simpan
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
---
+++ src/utilities/data.js
@@ -0,0 +1,98 @@
+const mahasiswa = [
+  {
+    title: 'Mahasiswa 1',
+    name: 'mahasiswa_1'
+  },
+  {
+    title: 'Mahasiswa 2',
+    name: 'mahasiswa_2'
+  },
+  {
+    title: 'Mahasiswa 3',
+    name: 'mahasiswa_3'
+  },
+  {
+    title: 'Mahasiswa 4',
+    name: 'mahasiswa_4'
+  },
+  {
+    title: 'Mahasiswa 5',
+    name: 'mahasiswa_5'
+  },
+  {
+    title: 'Mahasiswa 6',
+    name: 'mahasiswa_6'
+  },
+  {
+    title: 'Mahasiswa 7',
+    name: 'mahasiswa_7'
+  },
+  {
+    title: 'Mahasiswa 8',
+    name: 'mahasiswa_8'
+  },
+  {
+    title: 'Mahasiswa 9',
+    name: 'mahasiswa_9'
+  },
+  {
+    title: 'Mahasiswa 10',
+    name: 'mahasiswa_10'
+  }
+]
+
+const nilai = [
+  {
+    name: 'nilai',
+    value: 0
+  },
+  {
+    name: '1',
+    value: 1
+  },
+  {
+    name: '2',
+    value: 2
+  },
+  {
+    name: '3',
+    value: 3
+  },
+  {
+    name: '4',
+    value: 4
+  },
+  {
+    name: '5',
+    value: 5
+  },
+  {
+    name: '6',
+    value: 6
+  },
+  {
+    name: '7',
+    value: 7
+  },
+  {
+    name: '8',
+    value: 8
+  },
+  {
+    name: '9',
+    value: 9
+  },
+  {
+    name: '10',
+    value: 10
+  }
+]
+
+const titleAspek = [
+  {title: `Aspek \n Penilaian 1`, name: 'aspek_penilaian_1'},
+  {title: `Aspek \n Penilaian 2`, name: 'aspek_penilaian_2'},
+  {title: `Aspek \n Penilaian 3`, name: 'aspek_penilaian_3'},
+  {title: `Aspek \n Penilaian 4`, name: 'aspek_penilaian_4'}
+]
+
+export {mahasiswa, nilai, titleAspek}
